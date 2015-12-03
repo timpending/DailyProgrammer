3d62f9522414bf1925e1a35763695e61d210cef9
Ben Allred <skuttleman@gmail.com>
2015-12-03 13:53:49 -0700

    06 shorty

--- 06_longestString/solutions/bda.js
+++ 06_longestString/solutions/bda.js
@@ -4,9 +4,7 @@ function longestString(array) {
   var answer = array.sort(function(word1, word2) {
     return word2.length - word1.length;
   })[0];
-  var ret = {};
-  ret[answer] = answer.length;
-  return ret;
+  return {[answer]: answer.length};
 }
 
 console.log(longestString(["truck", "sidewalk", "book"]));
