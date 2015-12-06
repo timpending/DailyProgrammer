001cecd3aa03868ac288300e92cb6c629428aa58
Danny Fritz <dannyfritz@gmail.com>
2015-12-05 22:36:43 -0700

    Update Danny.js

--- 06_longestString/solutions/Danny.js
+++ 06_longestString/solutions/Danny.js
@@ -1,5 +1,5 @@
 const sortLength = (array) => array.sort((a, b) => a.length < b.length)
-const format = (str) => {return {[str]: str.length}}
+const format = (str) => ({[str]: str.length})
 const longestString = (strArray) => format(sortLength(strArray)[0])
 
 console.log(longestString(['truck', 'sidewalk', 'book']))
