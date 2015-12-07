0c17373a771166143070883d173688baa07f8d2f
Ben Allred <skuttleman@gmail.com>
2015-12-07 13:19:27 -0700

    Simplify 08

--- 08_combineAndSort/solutions/bda.js
+++ 08_combineAndSort/solutions/bda.js
@@ -1,7 +1,5 @@
 function combineAndSort(array1, array2) {
-  return array1.concat(array2).sort(function(word1, word2) {
-    return word1 > word2;
-  });
+  return array1.concat(array2).sort();
 }
 
 console.log(combineAndSort([ "cat", "dog", "fish", "zebra" ], [ "lion", "aardvark", "gorilla" ]));
