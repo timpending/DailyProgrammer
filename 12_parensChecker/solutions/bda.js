858f9f5173c94dfa233d3e3b0df973afe9c0155c
Ben Allred <skuttleman@gmail.com>
2015-12-10 13:27:17 -0700

    12 done

---
+++ 12_parensChecker/solutions/bda.js
@@ -0,0 +1,16 @@
+function parensChecker(string) {
+  var depth = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === '(') depth++;
+    else if (string[i] === ')') depth--;
+    if (depth < 0) return false;
+  }
+  return true;
+}
+
+
+console.log(parensChecker("()(((())))")); //=> true
+
+console.log(parensChecker("())))(")); //=> false
+
+console.log(parensChecker(")(")); //=> false
