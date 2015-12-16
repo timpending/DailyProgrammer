c43dee2a80a36f2672fba2316995c5bdfdb4a8e6
davidtadams <adams.t.david@gmail.com>
2015-12-16 13:53:15 -0700

    DTA adding answer to 16

--- 16_zipArrays/solutions/DTA.js
+++ 16_zipArrays/solutions/DTA.js
@@ -2,35 +2,19 @@
 function zipArrays(arr1, arr2) {
   var retArr = [];
 
-  if (arr1.length < arr2.length) {
-    //arr1 is smaller
-    for (var i = 0; i < arr1.length; i++) {
-      retArr.push(arr1[i]);
-      retArr.push(arr2[i]);
-    }
-
-    //append rest of arr2
-    retArr = retArr.concat(arr2.slice(i));
-  }
-  else if (arr1.length > arr2.length) {
-    //arr2 is smaller
-    for (var i = 0; i < arr2.length; i++) {
-      retArr.push(arr1[i]);
-      retArr.push(arr2[i]);
-    }
-
-    //append rest of arr1
-    retArr = retArr.concat(arr1.slice(i));
-  }
-  else {
-    //arr1 length == arr2 length
-    for (var i = 0; i < arr1.length; i++) {
-      retArr.push(arr1[i]);
-      retArr.push(arr2[i]);
-    }
+  if (arr2.length < arr1.length) {
+    //arr2 is smaller, swap arr2 and arr1.
+    var temp = arr1;
+    arr1 = arr2;
+    arr2 = temp;
   }
 
-  return retArr;
+  for (var i = 0; i < arr1.length; i++) {
+    retArr.push(arr1[i]);
+    retArr.push(arr2[i]);
+  }
+
+  return retArr.concat(arr2.slice(i));
 }
 
 console.log(zipArrays([1,2,3], [4,5,6]));       //[1,4,2,5,3,6]
