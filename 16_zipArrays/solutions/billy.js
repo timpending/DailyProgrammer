54027511b6955242682956f74fad0b8fbd3bd9cb
William Johnson <wjohnson1000@gmail.com>
2015-12-16 13:29:50 -0700

    16 answered

---
+++ 16_zipArrays/solutions/billy.js
@@ -0,0 +1,12 @@
+function zipArray(array1, array2){
+  var zipped = [];
+  var a, b = 0;
+  for (i=0; i<array1.length; i++){
+    zipped.push(array1[i]);
+    zipped.push(array2[i]);
+  }
+  return zipped;
+}
+
+
+console.log(zipArray([1,2,3], [4,5,6]));
