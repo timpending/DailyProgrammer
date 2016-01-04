3deac60b763cffb6b55b6a8e4cbbe824c1de5661
CJ R <hello@cjr.co.de>
2016-01-04 13:28:35 -0700

    add CJ solution to 20

---
+++ 20_sumDigits/solutions/CJ.js
@@ -0,0 +1,39 @@
+function getNumberLength(value) {
+  return Math.max(Math.floor(Math.log10(Math.abs(value))), 0) + 1;
+}
+
+function sumDigitsMaths(value) {
+  var sum = 0;
+  while(value > 0) {
+    sum += value % 10;
+    value = Math.floor(value / 10);
+
+    if(value == 0 && getNumberLength(sum) > 1) {
+      value = sum;
+      sum = 0;
+    }
+  }
+
+  return sum;
+}
+
+function sumDigits(value) {
+  value = Math.abs(value);
+  var sum =  value.toString().split('').reduce(function(sum, current) {
+    return sum + Number(current);
+  }, 0);
+
+  if(sum.toString().length > 1) {
+    return sumDigits(sum);
+  } else {
+    return sum;
+  }
+}
+
+console.log(sumDigits(12))
+console.log(sumDigits(12345))
+console.log(sumDigits(667))
+
+console.log(sumDigitsMaths(12))
+console.log(sumDigitsMaths(12345))
+console.log(sumDigitsMaths(667))
