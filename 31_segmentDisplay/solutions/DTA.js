2480585b97d19993df92aa574744f2c27fa62bbd
davidtadams <adams.t.david@gmail.com>
2016-02-25 10:03:08 -0700

    refactoring solution to 31

--- 31_segmentDisplay/solutions/DTA.js
+++ 31_segmentDisplay/solutions/DTA.js
@@ -1,51 +1,51 @@
 /* Created by David Adams February 24 2016 */
 var numbers = {
-  one: {
+  '1': {
     line1: '   ',
     line2: '  |',
     line3: '  |'
   },
-  two: {
+  '2': {
     line1: ' __',
     line2: ' _|',
     line3: '|__'
   },
-  three: {
+  '3': {
     line1: '__ ',
     line2: '__|',
     line3: '__|'
   },
-  four: {
+  '4': {
     line1: '   ',
     line2: '|_|',
     line3: '  |'
   },
-  five: {
+  '5': {
     line1: ' _ ',
     line2: '|_ ',
     line3: ' _|'
   },
-  six: {
+  '6': {
     line1: ' _ ',
     line2: '|_ ',
     line3: '|_|'
   },
-  seven: {
+  '7': {
     line1: '__ ',
     line2: '  |',
     line3: '  |'
   },
-  eight: {
+  '8': {
     line1: ' _ ',
     line2: '|_|',
     line3: '|_|'
   },
-  nine: {
+  '9': {
     line1: ' _ ',
     line2: '|_|',
     line3: ' _|'
   },
-  zero: {
+  '0': {
     line1: ' _ ',
     line2: '| |',
     line3: '|_|'
@@ -59,69 +59,14 @@ function sevenSegment(num) {
     line2: '',
     line3: ''
   }
-  var printOutput = ''
 
   for(var i = 0; i < numList.length; i++) {
-    switch (numList[i]) {
-      case '0':
-        printSevenSegment.line1 += numbers.zero.line1
-        printSevenSegment.line2 += numbers.zero.line2
-        printSevenSegment.line3 += numbers.zero.line3
-        break;
-      case '1':
-        printSevenSegment.line1 += numbers.one.line1
-        printSevenSegment.line2 += numbers.one.line2
-        printSevenSegment.line3 += numbers.one.line3
-        break;
-      case '2':
-        printSevenSegment.line1 += numbers.two.line1
-        printSevenSegment.line2 += numbers.two.line2
-        printSevenSegment.line3 += numbers.two.line3
-        break;
-      case '3':
-        printSevenSegment.line1 += numbers.three.line1
-        printSevenSegment.line2 += numbers.three.line2
-        printSevenSegment.line3 += numbers.three.line3
-        break;
-      case '4':
-        printSevenSegment.line1 += numbers.four.line1
-        printSevenSegment.line2 += numbers.four.line2
-        printSevenSegment.line3 += numbers.four.line3
-        break;
-      case '5':
-        printSevenSegment.line1 += numbers.five.line1
-        printSevenSegment.line2 += numbers.five.line2
-        printSevenSegment.line3 += numbers.five.line3
-        break;
-      case '6':
-        printSevenSegment.line1 += numbers.six.line1
-        printSevenSegment.line2 += numbers.six.line2
-        printSevenSegment.line3 += numbers.six.line3
-        break;
-      case '7':
-        printSevenSegment.line1 += numbers.seven.line1
-        printSevenSegment.line2 += numbers.seven.line2
-        printSevenSegment.line3 += numbers.seven.line3
-        break;
-      case '8':
-        printSevenSegment.line1 += numbers.eight.line1
-        printSevenSegment.line2 += numbers.eight.line2
-        printSevenSegment.line3 += numbers.eight.line3
-        break;
-      case '9':
-        printSevenSegment.line1 += numbers.nine.line1
-        printSevenSegment.line2 += numbers.nine.line2
-        printSevenSegment.line3 += numbers.nine.line3
-        break;
-      default:
-        //do nothing
-    }
-
-    printSevenSegment.line1 += '  '
-    printSevenSegment.line2 += '  '
-    printSevenSegment.line3 += '  '
+    printSevenSegment.line1 += numbers[numList[i]].line1 + '  '
+    printSevenSegment.line2 += numbers[numList[i]].line2 + '  '
+    printSevenSegment.line3 += numbers[numList[i]].line3 + '  '
   }
 
+  var printOutput = ''
   for (var key in printSevenSegment) {
     printOutput += printSevenSegment[key] + '\n'
   }
