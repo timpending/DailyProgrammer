c91ee39011f03f94d82f77a5fb620a8e4df6514a
Tim Pender <mrtimpender@gmail.com>
2016-12-26 11:41:03 -0500

    adding #1

---
+++ 01_countLetters/count-letters.js
@@ -0,0 +1,17 @@
+function letterCount(str) {
+  str = str.replace(/\W/g, '').replace(/[0-9]+/g, '').toLowerCase();
+
+  let obj = {}
+  for (let i=0; i<str.length; i++){
+    if (obj[str.charAt(i)]){
+      obj[str.charAt(i)]++
+    } else {
+      obj[str.charAt(i)] = 1
+    }
+  }
+  return obj
+}
+
+let string = "The quick brown fox jumps over the lazy dog and the sleeping cat early in the day."
+
+console.log(letterCount(string));
