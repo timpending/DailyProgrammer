3747b5f37ac7d5325e089ef92f55a2897e393840
Tim Pender <mrtimpender@gmail.com>
2017-01-03 20:48:26 -0700

    palindrome test initiated

--- 03_palindrome/palindrome.js
+++ 03_palindrome/palindrome.js
@@ -7,20 +7,20 @@
 // Split edited string into array, reverse order, join
 // test if reversed,joined array is equal to edited string
 
-function palindrome(string){
+function Palindrome() {};
+
+Palindrome.prototype.pTest = function(string){
   let stripString = string.match(/[a-zA-Z]+/g).join('').toLowerCase();
   let reverseString = stripString.split('').reverse().join('').toLowerCase();
   return stripString === reverseString
 }
 
+// console.log(palindrome('hello'));
+// console.log(palindrome('race car'));
+// console.log(palindrome('Amor, Roma'));
+// console.log(palindrome('A man, a plan, a canal, Panama!'));
+// console.log(palindrome('step on no pets'));
+// console.log(palindrome('Was it a car or a cat I saw?'));
+// console.log(palindrome("No 'x' in Nixon"));
 
-console.log(palindrome('hello'));
-console.log(palindrome('race car'));
-console.log(palindrome('Amor, Roma'));
-console.log(palindrome('A man, a plan, a canal, Panama!'));
-console.log(palindrome('stack cats'));
-console.log(palindrome('taco cat'));
-console.log(palindrome('step on no pets'));
-console.log(palindrome('Was it a car or a cat I saw?'));
-console.log(palindrome("No 'x' in Nixon"));
-console.log(palindrome('put it up'));
+module.exports = Palindrome;
--- 03_palindrome/test.js
+++ 03_palindrome/test.js
@@ -1,7 +1,37 @@
 var expect = require('chai').expect;
+var Palindrome = require('./palindrome')
 
-describe('example test', function(){
-  it('pass true when true', function () {
-    expect(true).to.be.true;
+describe('hello', function(){
+  it('returns false', function () {
+    let test = new Palindrome();
+    expect(test.pTest('hello')).to.be.false;
+  });
+});
+
+describe('race car', function(){
+  it('returns false', function () {
+    let test = new Palindrome();
+    expect(test.pTest('race car')).to.be.true;
+  });
+});
+
+describe('Amor, Roma', function(){
+  it('returns true, ignores comma', function () {
+    let test = new Palindrome();
+    expect(test.pTest('Amor, Roma')).to.be.true;
+  });
+});
+
+describe('A man, a plan, a canal, Panama!', function(){
+  it('returns true, ignores punctuation', function () {
+    let test = new Palindrome();
+    expect(test.pTest('A man, a plan, a canal, Panama!')).to.be.true;
+  });
+});
+
+describe("No 'x' in Nixon", function(){
+  it('returns true, ignores comma, UpperCase N', function () {
+    let test = new Palindrome();
+    expect(test.pTest('Amor, Roma')).to.be.true;
   });
 });
